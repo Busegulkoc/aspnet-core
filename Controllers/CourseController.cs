62e94ddcc6c2b09204a2424f7d01b632322bfbbe
= <busegulkoc35@gmail.com>
2024-02-02 02:47:41 +0300

    routeLinks-Repository-DetailsPage

--- Controllers/CourseController.cs
+++ Controllers/CourseController.cs
@@ -10,29 +10,21 @@ namespace basics.Controllers
 {
     public class CourseController : Controller
     {
-        public IActionResult Index(){
-            var kurs = new Course
-            {
-                Id = 1,
-                Title = "Aspnet core kursu",
-                Description = "Güzel bir kurs",
-                Image="1.jpg"
-            };
+         public IActionResult Details(int? id){
+           
+           if(id == null){
+            return RedirectToAction("List","Course");
+           }
+           var kurs = Repository.GetById(id);
 
             return View(kurs);
         }
 
         public IActionResult List(){
-        var kurslar = new List<Course>()
-        {
-            new Course(){Id = 1, Title= "aspnet kursu" , Description="güzel bir kurs", Image= "1.jpg"},
-            new Course(){Id = 2, Title= "php kursu" , Description="güzel bir kurs", Image= "2.jpg"},
-            new Course(){Id = 3, Title= "js kursu" , Description="güzel bir kurs",  Image= "3.jpg"},
-
-        };
-            return View("list", kurslar);
+        
+            return View("list", Repository.Courses);
         }
 
-        
+
     }
 }
